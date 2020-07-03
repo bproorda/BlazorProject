31bc94cfd9acf3f4b17d2d344ec71e5b3dd6ee36
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-07-02 21:59:44 -0500

    added page to lists task from keiths demo api

---
+++ BlazorChat/BlazorChat.Blazor/Shared/taskModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorChat.Blazor.Shared
+{
+   public class taskModel
+    {
+        public string title { get; set; }
+
+        public int difficulty { get; set; }
+
+        public string assignedTo { get; set; }
+
+        public bool completed { get; set; }
+    }
+}
